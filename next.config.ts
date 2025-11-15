40e9f904ae82b28714ddd8bb1560024ce5746d76
hhk <1544832671@qq.com>
2025-11-16 07:35:12 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -3,34 +3,6 @@ import { NextConfig } from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.jsdelivr.net',
-        port: '',
-        pathname: '/gh/devicons/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'vitejs.dev',
-        port: '',
-        pathname: '/logo.svg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'jd-opensource.github.io',
-        port: '',
-        pathname: '/micro-app/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/**',
-      },
-    ],
-  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
 };
